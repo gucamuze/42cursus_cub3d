85cac10b9a13f14fb5c8684fdc5447c98f15fb84
Guillaume CAMUZEAUX <gucamuze@e3r3p9.clusters.42paris.fr>
2022-05-17 18:27:18 +0200

    abc

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gucamuze <gucamuze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 16:25:57 by gucamuze          #+#    #+#             */
-/*   Updated: 2022/05/17 18:01:27 by gucamuze         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:25:49 by gucamuze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,17 @@ int	main(int ac, char **av)
 	t_scene	*scene;
 	t_mlx	*mlx;
 
-	mlx = init_ui();
 	scene = malloc(sizeof(t_scene));
+	if (!parsing(ac, av, scene))
+		return (1);
+	mlx = init_ui();
 	if (!mlx)
 		return (1);
-	if (parsing(ac, av, scene))
-		printf("parsing ok");
 	init_img(mlx);
 	add_minimap(mlx);
 	mlx_put_image_to_window(mlx->ptr, mlx->window, mlx->img.img_ptr, 0, 0);
 	mlx_loop(mlx->ptr);
 	cleanup(mlx);
+	free(scene);
 	return (0);
 }
